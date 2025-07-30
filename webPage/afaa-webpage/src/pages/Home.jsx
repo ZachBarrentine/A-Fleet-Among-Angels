eb6c043916769c25f11be2e826ca2f1a05e38bc6
polanco04 <capgbm@gmail.com>
2025-07-29 22:17:05 -0600

    commented out the banner image tests

--- webPage/afaa-webpage/src/pages/Home.jsx
+++ webPage/afaa-webpage/src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home(){
     return ( 
     <> 
         <div className="TitleBox">
-            <img src="src/assets/Space1.jpg" className="bannerImg"></img>
+            {/*<img src="src/assets/Space1.jpg" className="bannerImg"></img>*/}
             <h1 className="homeTitle">A Fleet Among Angels</h1>
             <p className="homeSubtitle">Rebirth of a Galactic Peril</p>
         </div>
