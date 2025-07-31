4728fb02ac4dca23442d2a18c3c731c860056063
polanco04 <capgbm@gmail.com>
2025-07-30 19:20:06 -0600

    google sign in and project email

--- webPage/afaa-webpage/firebase.js
+++ webPage/afaa-webpage/firebase.js
@@ -4,18 +4,21 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCbWiK7q8Nh1-nYYPROPrgNZ8L1g5wIY5E",
-  authDomain: "angels-6c2db.firebaseapp.com",
-  projectId: "angels-6c2db",
-  storageBucket: "angels-6c2db.firebasestorage.app",
-  messagingSenderId: "312781630689",
-  appId: "1:312781630689:web:853169fbbe625853731ba0",
-  measurementId: "G-V1C9MTTM2T"
+  apiKey: "AIzaSyDWNIMHbms4-eZmJU1TyzopglWv9XmQ5Dc",
+  authDomain: "a-fleet-among-angels.firebaseapp.com",
+  projectId: "a-fleet-among-angels",
+  storageBucket: "a-fleet-among-angels.firebasestorage.app",
+  messagingSenderId: "771188935939",
+  appId: "1:771188935939:web:caeecf66172cb00be76d36"
 };
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+console.log(app);
+console.log(auth);
+console.log(db);
 
 export { auth, db };
--- webPage/afaa-webpage/src/pages/Demo.jsx
+++ webPage/afaa-webpage/src/pages/Demo.jsx
@@ -1,6 +1,12 @@
 import "../css/Demo.css"
+import {auth} from '../../firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'; // need to install using npm install react-firebase-hooks
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 function Demo(){
+    
+    const [user] = useAuthState(auth);
+    
     return (
         <div>
             <h1 className ="projectHeadline">Demo Placeholder</h1>
@@ -12,9 +18,24 @@ function Demo(){
                 <div className="demoScreen">
                 </div>
             </div>
-            <button className="playButton">PLAY DEMO</button>
+            <button className="playDemoButton">PLAY DEMO</button>
+            {user ? <SignOut /> : <SignIn />}
         </div>
     );
+}
+
+function SignIn(){
+    const signInWithGoogle = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider).catch((error) => {
+        console.error("Failed to sign in:", error)});
+    }
+    return <button className="signInButton" onClick={signInWithGoogle}>Sign In</button>
+}
+
+function SignOut(){
+    return (<button className="signOutButton" onClick={() => signOut(auth).catch((error) => {
+        console.error("Failed to sign out:", error)})}>Sign Out</button>)
 }
 
 export default Demo;
