d59555d56c276145e477dae0f7082a85526eb879
Jack Tsui <jacktsui23@gmail.com>
2025-08-03 16:47:22 -0500

    Implementing attack system.

---
+++ Game/constants.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+class TileType(Enum):
+
+    GRASS = "grass"
+    STONE = "stone"
+    WATER = "water"
+    MOUNTAIN = "mountain"
+
+class GridState(Enum):
+    IDLE = "idle"
+    UNIT_SELECT = "unit_select"
+    MOVING = "moving"
+    TARGETING = "targeting"
+    DECISION = "decision"
---
+++ Game/unit.py
@@ -0,0 +1,27 @@
+
+from typing import Tuple
+
+class Unit:
+    def __init__(self, name: str, grid_pos: Tuple[int, int], movement_range: int = 3, team: str = "player", hp=100, attack=20):
+        self.name = name
+        self.grid_pos = grid_pos
+        self.movement_range = movement_range
+        
+        self.has_moved = False
+        self.team = team    # "ally", "enemy"
+        self.sprite = None  # pygame surface
+
+        self.hp = hp
+        self.max_hp = hp
+        self.attack = attack
+        
+    def get_pixel_pos(self, tile_size:int) -> Tuple[int, int]:
+
+
+        # Convert tile unit to pixel unit
+        return (self.grid_pos[0] * tile_size, self.grid_pos[1] * tile_size)
+
+
+    def can_move(self):
+        return not self.has_moved
+    
