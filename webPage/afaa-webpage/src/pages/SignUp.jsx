802188b360ea8f148626c64044cbebb96dfafe59
polanco04 <capgbm@gmail.com>
2025-08-06 16:33:35 -0600

    implemented sign in and sign up

--- webPage/afaa-webpage/src/pages/SignUp.jsx
+++ webPage/afaa-webpage/src/pages/SignUp.jsx
@@ -1,35 +1,62 @@
 import {auth} from '../../firebase'
-import { useAuthState } from 'react-firebase-hooks/auth'; // need to install using npm install react-firebase-hooks
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { createUserWithEmailAndPassword, validatePassword } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
 import "../css/SignUp.css"
 
-function SignInPage(){
+function SignUpWithUAndP(){
+   
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSignUp = async (e) => {
     
-    const [user] = useAuthState(auth);
-    
+        e.preventDefault();
+
+        const status = await validatePassword(auth, password);
+
+        if(!status.isValid){
+            alert("Password must contain an uppercase letter and a special character (_, /, -).");
+            return
+        }
+
+        try{
+            await createUserWithEmailAndPassword(auth, email, password)
+            navigate('/Demo');
+        }
+        catch(error){
+            console.error(error.message);
+        };
+    }
+  
     return (
         <div className='mainUpBox'>
         <div className='signUpBox'>
             <h1 className ="signUpTitle">A Fleet Among Angels</h1>
             
-            <form>
+            <form onSubmit={handleSignUp}>
 
             <input type="email"
+                   value={email}
                    className="emailUp"
                    placeholder="Email"
+                   onChange={(e) => setEmail(e.target.value)}
                    required/>
             
             <br></br>
             
             <input type="password" 
+                   value={password}
                    className="passwordUp"
                    placeholder='Password'
+                   onChange={(e) => setPassword(e.target.value)}
                    required/>
 
             <br></br>
 
-            <button type='submit'
+            <button 
+                   type='submit'
                    className='submitUp'>Sign Up</button>
             </form>
         </div>
@@ -37,8 +64,4 @@ function SignInPage(){
     );
 }
 
-function SignInWithUAndP(){
-    return <button className="signInButton">Sign In With Username And Password</button>
-}
-
-export default SignInPage;
+export default SignUpWithUAndP;
