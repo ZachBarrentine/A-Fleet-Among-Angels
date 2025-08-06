8d9d03fb2800ab11f7d416e6ad3c937500a45892
polanco04 <capgbm@gmail.com>
2025-08-05 19:38:10 -0600

    sign in and sign up pages

--- webPage/afaa-webpage/src/App.jsx
+++ webPage/afaa-webpage/src/App.jsx
@@ -1,22 +1,30 @@
 import {Routes, Route} from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import './css/App.css'
 import NavBar from './components/NavBar'
 import AboutTheProject from './pages/AboutTheProject'
 import Progress from './pages/Progress'
 import Home from './pages/Home'
 import Demo from './pages/Demo'
+import SignInPage from './pages/SignIn.jsx'
+import SignUpPage from './pages/SignUp.jsx'
 import '../firebase.js'
 
+
 function App() {
-  //const [count, setCount] = useState(0)
+  const location = useLocation();
+  const hideNavBar = ['/SignIn', '/SignUp'];
+
   return (
     <>
-      <NavBar />      
+      {!hideNavBar.includes(location.pathname) && <NavBar />}   
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/AboutTheProject" element={<AboutTheProject />}></Route>
         <Route path="/Progress" element={<Progress />}></Route>
         <Route path="/Demo" element={<Demo />}></Route>
+        <Route path="/SignIn" element={<SignInPage />}></Route>
+        <Route path="/SignUp" element={<SignUpPage />}></Route>
       </Routes>
     </>
   );
--- webPage/afaa-webpage/src/pages/Demo.jsx
+++ webPage/afaa-webpage/src/pages/Demo.jsx
@@ -1,7 +1,8 @@
 import "../css/Demo.css"
 import {auth} from '../../firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'; // need to install using npm install react-firebase-hooks
-import { GoogleAuthProvider, signInWithPopup, signOut, signInWithEmailAndPassword } from "firebase/auth";
+import {signOut} from "firebase/auth";
+import {Link} from "react-router-dom";
 
 function Demo(){
     
@@ -19,25 +20,11 @@ function Demo(){
                 </div>
             </div>
             <div className = "signNplay">
-            {user ? (<div className = "demoSignOut"><button className="playDemoButton">PLAY DEMO</button><SignOut /> </div>) : 
-                    ( <><SignInWithG /> <button className="playDemoButton">PLAY DEMO</button> <SignInWithUAndP /> </>)}
+            <button className="playDemoButton">PLAY DEMO</button>
+            {user ? <SignOut /> : <Link to = "/SignIn" className = "signInButton">Sign In</Link>}
             </div>
         </div>
     );
-}
-
-function SignInWithG(){
-    const signInWithGoogle = () => {
-        const provider = new GoogleAuthProvider();
-        signInWithPopup(auth, provider).catch((error) => {
-        console.error("Failed to sign in:", error)});
-    }
-    return <button className="signInGButton" onClick={signInWithGoogle}>Sign In With Google</button>
-}
-
-function SignInWithUAndP(){
-    // Placeholder for future un and pw sign-in
-    return <button className="signInButton">Sign In With Username And Password</button>
 }
 
 function SignOut(){
