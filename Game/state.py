e454c17fc885c03faac26aa54125ab7f4d7a84a9
Jack Tsui <jacktsui23@gmail.com>
2025-08-09 15:14:33 -0500

    Made state manager and creating the screens for the game.

---
+++ Game/state.py
@@ -0,0 +1,59 @@
+
+from Game.screens import title
+import pygame
+from Game.constants import  SCREEN_WIDTH, SCREEN_HEIGHT
+
+class State:
+
+    def __init__(self, ui):
+        self.ui = ui # All the UI components in state.
+        self.is_active = False
+
+    def draw(self, screen):
+
+        # Cannot just use .draw because the bacground is a pygame image
+        # that uses .blit not draw like the other UI components.
+        if "background" in self.ui: 
+            screen.blit(self.ui["background"], (0,0))
+
+        for key, component in self.ui.items():
+            if key != "background":
+                component.draw(screen)
+
+
+    def handle_event(self, event):
+        pass
+
+class State_Manager:
+
+    def __init__(self):
+        self.states = {
+            "title": State(title)
+        }
+
+        self.current_state = "title"
+
+    def switch_states(self, next_state):
+        self.current_state = next_state
+
+    
+    def check_current_state(self):
+
+        return self.current_state
+    
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("A Fleet Among Angles")
+        
+state_manager = State_Manager()
+running = True
+
+while running:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+             running = False
+
+    state_manager.states[state_manager.current_state].draw(screen)
+    pygame.display.flip()
