8666b6a5eadf2b58c5945ba1f29fc265f1ce092d
polanco04 <capgbm@gmail.com>
2025-09-27 03:04:32 -0500

    logo

--- Game/save.py
+++ Game/save.py
@@ -1,13 +1,10 @@
-
 import json
-
 
 def save(data, path):
     with open(path, "w") as f:
         json.dump(data, f, indent=4)
 
 def load(path):
-
     with open(path, "r") as f:
         data = json.load(f)
 
