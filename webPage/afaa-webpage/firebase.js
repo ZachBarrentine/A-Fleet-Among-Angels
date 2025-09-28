4a7258b4d014edb1b7ab1e00d9a8ccb9303fcb3a
polanco04 <capgbm@gmail.com>
2025-09-28 18:25:07 -0500

    api key...

--- webPage/afaa-webpage/firebase.js
+++ webPage/afaa-webpage/firebase.js
@@ -5,12 +5,6 @@ import { getFirestore } from "firebase/firestore";
 
 // Delete contents for every push
 const firebaseConfig = {
-    apiKey: "AIzaSyCzE4wbXqyNVyfZ3RWWOJSVatjXLEz2pQ8",
-    authDomain: "a-fleet-among-angels.firebaseapp.com",
-    projectId: "a-fleet-among-angels",
-    storageBucket: "a-fleet-among-angels.firebasestorage.app",
-    messagingSenderId: "771188935939",
-    appId: "1:771188935939:web:0d5c97e0c365acbae76d36"
 };
 
 const app = initializeApp(firebaseConfig);
